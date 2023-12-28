c225eb770e98b7ebfed4aab2f174857d8d7245c1
brandonsnover <144728738+brandonsnover@users.noreply.github.com>
2023-12-28 15:22:18 -0600

    added seed data and tag_list method for program model

--- app/models/program.rb
+++ app/models/program.rb
@@ -1,4 +1,25 @@
 class Program < ApplicationRecord
   has_many :days
   belongs_to :user
+
+  def tag_list
+    count = 0
+    tag_array = []
+    while count < days.length
+      day = days[count]
+      count2 = 0
+      while count2 < day.exercises.length
+        exercise = day.exercises[count2]
+        count3 = 0
+        while count3 < exercise.tags.length
+          tag_array << exercise.tags[count3]
+          count3 += 1
+        end
+        count2 += 1
+      end
+      count += 1
+    end
+    filtered_array = tag_array.reject { |item| item["name"] == "Compound" || item["name"] == "Accessory" || item["name"] == "Upper Body" || item["name"] == "Lower Body" }
+    return filtered_array.uniq
+  end
 end
--- app/views/programs/_program.json.jbuilder
+++ app/views/programs/_program.json.jbuilder
@@ -1,5 +1,6 @@
 json.id program.id
 json.name program.name
+json.tag_list program.tag_list
 json.created_at program.created_at
 json.updated_at program.updated_at
 
